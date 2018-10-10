05de8ddf0f4ebc23e3769a25a5d7ca61e16b63ec
Jonathan <jtc7537@gmail.com>
2018-10-10 12:13:20 -0400

    date object

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
 $( document ).ready(function(){
     console.log('asdf')
     $.get('/api/employee-data', function(data) {
-        console.log('asdf')
-        console.log(JSON.parse(data) || err)
+        if(data){
+            data = JSON.parse(data)
+            for(key in data){
+                const t = new Date()
+                const d = new Date(data[key])
+                console.log(d)
+
+            }
+
+        } else {
+            console.log(err)
+        }
     })
 })
 
