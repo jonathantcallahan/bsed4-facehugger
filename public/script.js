878e4c000ac80303094c899f247ef268554d9bb5
Jonathan <jtc7537@gmail.com>
2018-10-10 14:34:07 -0400

    rounded numbers

--- public/script.js
+++ public/script.js
@@ -3,12 +3,15 @@ $( document ).ready(function(){
     $.get('/api/employee-data', function(data) {
         if(data){
             data = JSON.parse(data)
+            b = []
             for(key in data){
                 const t = new Date()
                 const d = new Date(data[key])
-                console.log(d)
-
+                const timeDif = Math.floor((t.getTime() - d.getTime()) / (1000 * 3600 * 24))
+                const roundDif = Math.round(timeDif / 50) * 50
+                b.push(roundDif)
             }
+            console.log(b)
 
         } else {
             console.log(err)
