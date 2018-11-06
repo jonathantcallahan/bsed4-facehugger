d581570b381ed580bb6fcd3e6382f09e450ec2c4
Jonathan <jtc7537@gmail.com>
2018-11-06 13:14:48 -0500

    older version of d3

--- controllers/api.js
+++ controllers/api.js
@@ -20,35 +20,6 @@ module.exports = (app) => {
         })
     };
 
-    const getEf = res => {
-        request('https://www.efaucets.com/?_vis_test_id=51&_vis_opt_random=0.4777546671009354&_vis_hash=ba7adeae48e3d7315920016960e0af05&_vis_opt_preview_combination=2', (err,response,body) => {
-            if(err) { return err };
-            const links = {
-                textLinks : {},
-                photoLinks: {}
-            };
-            const $ = cheerio.load(body);
-            $('li.navExtra > ul > li > a').each(function(i,e){
-                const title = $(this).text().trim()
-                const href = $(this).attr('href')
-                links.textLinks[title] = href
-            })
-            $('li > a > img').each(function(i,e) {
-                const title = $(this).next().text()
-                console.log($(this).attr('src'))
-                links.photoLinks[title] = { href: $(this).parent().attr('href'), src: $(this).attr('data-yo-src') }
-            })
-            console.log(links)
-            fs.writeFile('efaucets-data.txt', JSON.stringify(links), function(err){
-                err && console.log(err)
-                console.log('File saved!')
-            })
-            res.json().status(200)
-            //console.log(dates)
-            //res.json(JSON.stringify(dates))
-        })
-    }
-
     app.get('/api/employee-data', (req, res) => {
         console.log('request made to api')
         scr(res)
--- public/script.js
+++ public/script.js
@@ -9,11 +9,31 @@ $( document ).ready(function(){
                 const d = new Date(data[key])
                 const timeDif = Math.floor((t.getTime() - d.getTime()) / (1000 * 3600 * 24))
                 const roundDif = Math.round(timeDif / 50) * 50
-                b.push(roundDif)
+                //b.push(roundDif)
+                b.push(Math.round(timeDif / 30))
             }
         } else {
             console.log(err)
         }
+        console.log(d3)
+        const histogram = d3.layout.histogram()
+            .bins(100)(b)
+        // console.log(histogram)
+
+        const canvas = d3.select('#data').append('svg')
+            .attr('width', 500)
+            .attr('height', 500)
+
+        const bars = canvas.selectAll('.bar')
+            .data(histogram)
+            .enter()
+            .append('g')
+
+        bars.append('rect')
+            .attr('x', d => d.x * 5)
+            .attr('y', 0)
+            .attr('width', d => d.dx * 4)
+            .attr('height', d => d.y * 20)
     })
 })
 
